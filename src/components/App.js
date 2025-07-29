908df6eb881bf3323088797113a54acaaad7a9e6
Ashiful Alam <ashalamshuvo@gmail.com>
2025-07-29 19:46:14 +0600

    Add state and logic to FormSplitBill inputs
    Introduced state management for bill value, user's payment, and payer selection in the FormSplitBill component. Inputs are now controlled, and the friend's expense is dynamically calculated based on the bill and user's payment. Prevents user from entering an amount greater than the bill.

--- src/components/App.js
+++ src/components/App.js
@@ -155,22 +155,43 @@ function FormAddFriend ({onAddFriend}) {
 }
 
 function FormSplitBill ({selectedFriend}) {
+    const [bill, setBill] = useState('');
+    const [paidByUser, setPaidByUser] = useState('');
+    const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+    const paidByFriend = bill ? bill - paidByUser : "";
+
     return(
         <form className="form-split-bill">
             <h2>
                 Split a bill with {selectedFriend.name}
             </h2>
             <label>💰 Bill Value</label>
-            <input type="text" />
+            <input 
+                type="text" 
+                value={bill} 
+                onChange={(e) => setBill(Number(e.target.value))}
+            />
 
             <label>💀 Your expenses</label>
-            <input type="text" />
+            <input 
+                type="text"
+                value={paidByUser} 
+                onChange={(e) => setPaidByUser(
+                                    Number(e.target.value) > bill ? paidByUser :
+                                    Number(e.target.value)
+                )
+            }
+             />
 
-             <label>🐵 {selectedFriend.name}'s expense</label>
-            <input type="text" disabled />
+            <label>🐵 {selectedFriend.name}'s expense</label>
+            <input type="text" disabled value={paidByFriend}/>
 
             <label>💵 Who is paying the bill</label>
-            <select>
+            <select
+                value={whoIsPaying} 
+                onChange={(e) => setWhoIsPaying(e.target.value)}
+            >
                 <option value='user'>You</option>
                 <option value="friend">{selectedFriend.name}</option>
             </select>
