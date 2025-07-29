42e0e34b1a72136592283fc970e450d26b11822a
Ashiful Alam <ashalamshuvo@gmail.com>
2025-07-29 21:53:33 +0600

    Add split bill functionality
    Introduced the handleSplitBill function to update a friend's balance when splitting a bill. Passed the handler to FormSplitBill and implemented form submission logic to trigger balance updates and reset the selected friend.

--- src/components/App.js
+++ src/components/App.js
@@ -48,6 +48,12 @@ export default function App(){
         setShowAddFriend(false);
    }
 
+   function handleSplitBill(value){
+        setFriend(friends => friends.map(friend => friend.id === selectedFriend.id ? {...friend, balance: friend.balance + value}: friend))
+
+        setSelectedFriend(null)
+   }
+
     return(
         <div className="app">
             <div className="sidebar">
@@ -62,7 +68,11 @@ export default function App(){
                 <Button onClick={handleShowAddFriend}>{ showAddFriend ? "Close" : "Add Friend"}</Button>
             </div>
 
-            {selectedFriend && <FormSplitBill selectedFriend={selectedFriend}/>}
+            {selectedFriend && 
+                <FormSplitBill 
+                    selectedFriend={selectedFriend}
+                    onSplitBill = {handleSplitBill}
+                />}
         </div>
     )
 }
@@ -154,15 +164,25 @@ function FormAddFriend ({onAddFriend}) {
     );
 }
 
-function FormSplitBill ({selectedFriend}) {
+function FormSplitBill ({selectedFriend, onSplitBill}) {
+
+    
+
     const [bill, setBill] = useState('');
     const [paidByUser, setPaidByUser] = useState('');
     const [whoIsPaying, setWhoIsPaying] = useState("user");
 
     const paidByFriend = bill ? bill - paidByUser : "";
 
+    function handleSubmit(e){
+        e.preventDefault();
+
+        if(!bill || !paidByUser) return;
+        onSplitBill(whoIsPaying === 'user' ? paidByFriend : -paidByUser);
+    }
+
     return(
-        <form className="form-split-bill">
+        <form className="form-split-bill" onSubmit={handleSubmit}>
             <h2>
                 Split a bill with {selectedFriend.name}
             </h2>
